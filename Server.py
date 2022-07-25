62374cf73407b345e021324f46ffb2c59d018c05
kiranbahadurbam <91513491+kiranbahadurbam@users.noreply.github.com>
2022-07-25 18:24:48 +0545

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,9 +1,8 @@
 # SERVER CODE
-# Update 1.1 AES Encryption Now Included :p
 # AES Block Cipher 16 bytes block size as supported by pyaes
 # You Can Change the Port Server Listens by passing argument in command line directly
 # Server Code To be Started before Client, or Connection will be refused
-# Author : xtreme.research@gmail.com
+# Author : kiranbam99@gmail.com
 
 import os
 try:
